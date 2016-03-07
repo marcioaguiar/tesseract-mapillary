a2ec60787223b9c1493ec67fd1cc2d9ab97545f5
Alexandre Magno <alexandre.mbm@gmail.com>
2016-03-07 13:35:28 -0300

    tesmap.sh skeleton ‒ ref. #1

---
+++ tesmap.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Tudo será em inglês? Por enquanto assumo que sim. Esses comentários em português são apenas um esqueleto inicial.
+
+# TODO importar variáveis com "source"?
+
+# TODO licença (somente comentário)
+
+# TODO descrição (somente comentário)
+
+# TODO dependências
+
+function check_depends() {
+
+}
+
+# pacman -Ss youtube-dl
+# pacman -Ss tesseract
+# pacman -Ss imagemagick
+# pacman -Ss ffmpeg
+
+# TODO funções auxiliares
+
+function extract_frames() {  # TODO
+    # Extraindo os quadros do vídeo (um por segundo)
+}
+
+function reduce1() {  # TODO
+    # Cortando a parte com a camada dos dados e o capô do carro
+}
+
+function reduce2() {  # TODO
+    # Cortando as coordenadas
+}
+
+function reduce3() {  # TODO
+    # Cortando as datas
+}
+
+function crop_frames() {  # TODO
+    # call reduce1
+    # call reduce2
+    # call reduce3
+    
+    # TODO renomear as três funcões acima, se for o caso
+}
+
+# TODO funções principais
+
+function download_video() {  # TODO
+
+}
+
+function adjust_constrast() {  # TODO (optional use)
+
+}
+
+function extract_data() {  # TODO
+
+}
+
+# TODO parsing
+
+# info
+# download
+# adjust (optional)
+# make
+# all
+# help
