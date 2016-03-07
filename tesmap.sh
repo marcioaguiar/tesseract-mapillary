0fc0a41d9d37b333317be326c5fced152b0a1ba8
Alexandre Magno <alexandre.mbm@gmail.com>
2016-03-07 15:11:08 -0300

    Checking of dependencies

--- tesmap.sh
+++ tesmap.sh
@@ -8,33 +8,59 @@
 
 # TODO descrição (somente comentário)
 
-# TODO dependências
+# Check dependencies
 
-function check_depends() {
-
+function is_ok() {
+    which $1 > /dev/null 2>&1
+    return $?
 }
 
-# pacman -Ss youtube-dl
-# pacman -Ss tesseract
-# pacman -Ss imagemagick
-# pacman -Ss ffmpeg
+function passing() {
+    is_ok $1 || ( echo "Lacks dependency: $1" && exit 1 )
+}
+
+function dependencies_check()
+{
+    passing "youtube-dl"
+    passing "ffmpeg"
+}
+
+dependencies_check
+
+# On Arch:
+#
+# pacman -S youtube-dl
+# pacman -S tesseract
+# pacman -S imagemagick
+# pacman -S ffmpeg
+#
+# On Ubuntu:
+#
+# apt-get install youtube-dl
+# apt-get install tesseract-ocr
+# apt-get install imagemagick
+# apt-get install libav-tools
 
 # TODO funções auxiliares
 
 function extract_frames() {  # TODO
     # Extraindo os quadros do vídeo (um por segundo)
+    echo
 }
 
 function reduce1() {  # TODO
     # Cortando a parte com a camada dos dados e o capô do carro
+    echo
 }
 
 function reduce2() {  # TODO
     # Cortando as coordenadas
+    echo
 }
 
 function reduce3() {  # TODO
     # Cortando as datas
+    echo
 }
 
 function crop_frames() {  # TODO
@@ -43,20 +69,21 @@ function crop_frames() {  # TODO
     # call reduce3
     
     # TODO renomear as três funcões acima, se for o caso
+    echo
 }
 
 # TODO funções principais
 
 function download_video() {  # TODO
-
+    echo
 }
 
 function adjust_constrast() {  # TODO (optional use)
-
+    echo
 }
 
 function extract_data() {  # TODO
-
+    echo
 }
 
 # TODO parsing
